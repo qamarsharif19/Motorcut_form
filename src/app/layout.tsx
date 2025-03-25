cbd8e7c32f824b8efc2d6ae7c9875d48b08eb579
qamarsharif19 <qamarwordpress@gmail.com>
2025-03-25 17:49:16 +0500

    font manrope

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Manrope } from 'next/font/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,7 +12,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
+const manrope = Manrope({ subsets: ["latin"], weight: ["300", "400", "500", "600", "700"] });
 export const metadata: Metadata = {
   title: "Motorcut Custom Form",
   description: "Custom form for leads",
