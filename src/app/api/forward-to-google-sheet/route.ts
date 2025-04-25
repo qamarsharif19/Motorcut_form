8e622077ed93772a7eb2b63f7c07e8fcbc2fcf21
qamarsharif19 <qamarwordpress@gmail.com>
2025-04-25 08:34:43 +0500

    url updated

--- src/app/api/forward-to-google-sheet/route.ts
+++ src/app/api/forward-to-google-sheet/route.ts
@@ -2,7 +2,7 @@ export async function POST(req: Request) {
     const data = await req.json();
   
     try {
-      const sheetResponse = await fetch("https://script.google.com/macros/s/AKfycbynsIU8J_uXsaFY4hNSSVNRp5g9HC5x2ujqZzZIyY-O3uatyw0FerQoU8y1MOcrSW81/exec", {
+      const sheetResponse = await fetch("https://script.google.com/macros/s/AKfycbw8XNMnJ1Hhar35i3Vc9MojQlJFgqRMWJFS2hjR0ZjDIvc2xrzebGSXUQLD3DxfdIPp/exec", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
